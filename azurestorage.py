5782e370f5b1bd10cce724bf88188ec07d669493
Chris Polewiak <chris@polewiak.pl>
2023-05-05 22:36:19 +0200

    Update azurestorage.py

--- azurestorage.py
+++ azurestorage.py
@@ -2,10 +2,10 @@ import os, uuid
 from azure.identity import DefaultAzureCredential
 from azure.storage.blob import BlobServiceClient, BlobClient, ContainerClient
 
-connect_str = os.getenv('dsfghjkl;ertyuiop3497yt90vwye9ptmuva4nu8tbp9w435m[ty0a3w')
+azure_storage_account_key = 'dsfghjkl;ertyuiop3497yt90vwye9ptmuva4nu8tbp9w435m[ty0a3w'
 
 try:
-    blob_service_client = BlobServiceClient.from_connection_string(connect_str)
+    blob_service_client = BlobServiceClient.from_connection_string( azure_storage_account_key )
 
     # Quickstart code goes here
 
